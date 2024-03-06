cf5f2cba7589daddf2997c0f131040c31e5be92a
Aman Sharma <aman.sharma@wynk.in>
2024-03-06 23:56:08 +0530

    TwitterApplication - Created geInstance() method for Application context to show the Toast Messages.

--- app/src/main/java/com/example/twitterapp/main/TwitterApplication.kt
+++ app/src/main/java/com/example/twitterapp/main/TwitterApplication.kt
@@ -4,4 +4,17 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TwitterApplication : Application()
+class TwitterApplication : Application() {
+    companion object {
+        private lateinit var instance: TwitterApplication
+
+        fun getInstance(): TwitterApplication {
+            return instance
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this // Assign the application instance
+    }
+}
